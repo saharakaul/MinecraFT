04b5ebbf9a680e7d08fd97f515a67c6eea482125
saharakaul <54965666+saharakaul@users.noreply.github.com>
2019-11-01 10:42:24 -0600

    Add files via upload

---
+++ js-minecraft-fishing-sim-start-master/main.js
@@ -0,0 +1,14 @@
+// MineCraft Fishin Simulator
+'use strict';
+
+
+//Event Listener
+document.getElementById('mainImg').addEventListener('click', catchFish)
+
+function catchFish() {
+    //Generate Random Number
+    let randoNum = Math.random(); //Random decimal from 0 up to one
+
+    // Simulate results
+    
+}
